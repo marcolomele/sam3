d602b35f9813cca75d209c5189fcfb7ea152dd8a
Marco Lomele <mark.lomele@gmail.com>
2026-03-20 14:12:55 +0100

    modified sh

--- run_mask_propagation.sh
+++ run_mask_propagation.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 #SBATCH --job-name=mask_prop
 #SBATCH --account=3152128
-#SBATCH --partition=gpu,gpunew
-#SBATCH --gpus-per-task=1
+#SBATCH --partition=gpunew
+#SBATCH --gpus=1
 #SBATCH --time=24:00:00
 #SBATCH --output=../slogs/%x_%j.out
 #SBATCH --error=../slogs/%x_%j.err
 #SBATCH --mail-user mark.lomele@gmail.com
 #SBATCH --mail-type END
 
+conda activate sam3_venv
+
 python examples/run_egoexo_propagation.py \
-    --data_dir /data/video_datasets/3321908/output_dir_all \
-    --input_csv mask_prop_input.csv \
-    --out_root /data/video_datasets/reluminati/propagation_results \
+    --data_dir ../data/video_datasets/3321908/output_dir_all \
+    --input_csv examples/mask_prop_input.csv \
+    --out_root propagation_results \
     --vis_stride 10 \
-    --confidence_threshold 0.3 \
-    --out_root /mask_prop_exp
+    --confidence_threshold 0.3
