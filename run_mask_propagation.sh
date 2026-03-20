99383ed60c81137dab75b95e772cdbc44d09ad95
Marco Lomele <mark.lomele@gmai.com>
2026-03-20 15:15:43 +0100

    fix directory bug

--- run_mask_propagation.sh
+++ run_mask_propagation.sh
@@ -5,8 +5,9 @@
 #SBATCH --gpus=1
 #SBATCH --mem=64G
 #SBATCH --time=24:00:00
-#SBATCH --output=../slogs/%j.out
-#SBATCH --error=../slogs/%j.err
+#SBATCH --chdir=/home/3152128/sam3
+#SBATCH --output=/home/3152128/slogs/%j.out
+#SBATCH --error=/home/3152128/slogs/%j.err
 #SBATCH --mail-user mark.lomele@gmail.com
 #SBATCH --mail-type END
 
@@ -16,7 +17,7 @@ eval "$(conda shell.bash hook)"
 conda activate sam3_venv
 
 python examples/run_egoexo_propagation.py \
-    --data_dir ../data/video_datasets/3321908/output_dir_all \
+    --data_dir /data/video_datasets/3321908/output_dir_all \
     --input_csv examples/mask_prop_input.csv \
     --out_root propagation_results \
     --vis_stride 10 \
